72e48c5db4cdb27b6936ffe9f1f176eef260a4f2
larzraz <lars5030@edu.ucl.dk>
2020-11-25 19:28:33 +0100

    refactored and added comments for document.php

--- document.php
+++ document.php
@@ -1,6 +1,7 @@
 <?php
     class Document
     {
+        // Henter hele filens content som en string. Først tjekker den dog lige om filen overhovedet eksistere.
         function getFileContent($fileName)
         {
             if (file_exists($fileName)) {
@@ -10,11 +11,12 @@
             }
             return $content;
         }
-
-        function parse($templateContent, $placeholderData)
+        // Parser content fra arrayet/objectet(overrideData varialen) over placeholderteksten i templatefilen. Dette gøres via et loop der looper alle forskellelige values man ønsker at parse.
+        // og str_replace finde så alle forekomster af placeholderen og erstatter den med den ønskede value. Når loopet er gennemkørt og alle placeholders er overskredet. returneres den nye templateContent med de overskrevne værdier
+        function parse($templateContent, $overrideData)
         {
-            foreach ($placeholderData as $placeholderKey => $placeholderValue) {
-                $templateContent = str_replace("{{{$placeholderKey}}}", $placeholderValue, $templateContent);
+            foreach ($overrideData as $placeholderKey => $overrideValue) {
+                $templateContent = str_replace("{{{$placeholderKey}}}", $overrideValue, $templateContent);
             }
             return $templateContent;
         }
--- index.php
+++ index.php
@@ -1,16 +1,22 @@
 <?php
     include ("document.php");
+    include ("ContentObject.php");
 
     $content = new Document();
 
     $contentTemplate = $content->getFileContent("template.html");
 
-    $placeholderData = array(
+    $overrideDataAsArray = array(
         "title" => "title_passed",
         "content" => "content_passed",
         "subcontent" => "subcontent_passed");
+    $overrideDataAsObject = new ContentObject();
 
-    $parsedContent = $content->parse($contentTemplate, $placeholderData);
+    $parsedContentFromArray = $content->parse($contentTemplate, $overrideDataAsArray);
+    $parsedContentFromArrayAndObject =  $content->parse($parsedContentFromArray, $overrideDataAsObject);
+    
+    echo $parsedContentFromArrayAndObject;
+    
+    
 
-    echo $parsedContent;
 ?>
